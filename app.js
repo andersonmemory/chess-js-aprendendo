f4f4c9e3c43a309e3bc6f014a503d9fb5eb3de75
andersonmemory <andersonleitedesa2100@gmail.com>
2024-05-19 06:13:23 -0300

    add pieces and board

---
+++ app.js
@@ -0,0 +1,16 @@
+const gameBoard = document.querySelector("#gameboard")
+const playerDisplay = document.querySelector("#player")
+const infoDisplay = document.querySelector("#info-display")
+const width = 8
+
+const startPieces = [
+  rook, knight, bishop, queen, king, bishop, knight, rook,
+  pawn, pawn, pawn, pawn, pawn, pawn, pawn, pawn,
+  '', '', '', '', '', '', '', '',
+  '', '', '', '', '', '', '', '',
+  '', '', '', '', '', '', '', '',
+  '', '', '', '', '', '', '', '',
+  pawn, pawn, pawn, pawn, pawn, pawn, pawn , pawn,
+  rook, knight, bishop, queen, king, bishop, knight, rook
+]
+
