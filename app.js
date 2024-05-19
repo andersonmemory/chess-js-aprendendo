473fc918653c25cb6740e4be209cb59c228fd0cf
andersonmemory <andersonleitedesa2100@gmail.com>
2024-05-19 14:09:46 -0300

    CreateBoard function added

--- app.js
+++ app.js
@@ -4,13 +4,86 @@ const infoDisplay = document.querySelector("#info-display")
 const width = 8
 
 const startPieces = [
-  rook, knight, bishop, queen, king, bishop, knight, rook,
-  pawn, pawn, pawn, pawn, pawn, pawn, pawn, pawn,
-  '', '', '', '', '', '', '', '',
-  '', '', '', '', '', '', '', '',
-  '', '', '', '', '', '', '', '',
-  '', '', '', '', '', '', '', '',
-  pawn, pawn, pawn, pawn, pawn, pawn, pawn , pawn,
-  rook, knight, bishop, queen, king, bishop, knight, rook
+  rook,
+  knight,
+  bishop,
+  queen,
+  king,
+  bishop,
+  knight,
+  rook,
+  pawn,
+  pawn,
+  pawn,
+  pawn,
+  pawn,
+  pawn,
+  pawn,
+  pawn,
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  pawn,
+  pawn,
+  pawn,
+  pawn,
+  pawn,
+  pawn,
+  pawn,
+  pawn,
+  rook,
+  knight,
+  bishop,
+  queen,
+  king,
+  bishop,
+  knight,
+  rook,
 ]
 
+function CreateBoard() {
+  startPieces.forEach((startPiece, i) => {
+    const square = document.createElement("div")
+    square.classList.add("square")
+    square.innerHTML = startPiece
+    square.firstChild?.setAttribute("draggable", true)
+    square.setAttribute("square-id", i)
+    let row = Math.floor(63 - i + 1)
+    if (row % 2 === 0) {
+      square.classList.add(i % 2 === 0 ? "beige" : "brown")
+    } else {
+      square.classList.add(i % 2 === 0 ? "brown" : brown)
+    }
+  })
+}
+
+CreateBoard()
