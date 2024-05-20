38e90845a48d1a2c8767e69ef3350201e798c942
andersonmemory <andersonleitedesa2100@gmail.com>
2024-05-20 11:45:57 -0300

    add more events

--- app.js
+++ app.js
@@ -14,29 +14,47 @@ const startPieces = [
   rook, knight, bishop, queen, king, bishop, knight, rook,
 ]
 
-function CreateBoard () {
+function CreateBoard() {
   startPieces.forEach((startPiece, i) => {
     const square = document.createElement("div")
     square.classList.add("square")
     square.innerHTML = startPiece
     square.firstChild?.setAttribute("draggable", true)
     square.setAttribute("square-id", i)
-    square.setAttribute("draggable", true)
-    const row = Math.floor(((63 - i) / 8) + 1)
+    const row = Math.floor((63 - i) / 8 + 1)
     if (row % 2 === 0) {
       square.classList.add(i % 2 === 0 ? "beige" : "brown")
-    } else { 
-      square.classList.add(i % 2 === 0 ? "brown" : "beige") 
-    }
-    if (i <= 15) {
-      square.firstChild?.firstChild?.classList.add("black")
-    }
-    if (i >= 45) {
-      square.firstChild?.firstChild?.classList.add("white")
-    }
-    gameBoard.append(square)
+      } else {
+        square.classList.add(i % 2 === 0 ? "brown" : "beige")
+      }
+      
+      if (i <= 15) {
+        square.firstChild.firstChild.classList.add("black")
+      }
+      if (i >= 48) {
+        square.firstChild.firstChild.classList.add("white")
+      }
+      gameBoard.append(square)
   })
-
 }
 CreateBoard()
 
+const allSquares = document.querySelectorAll("#gameboard .square")
+
+allSquares.forEach((square) => {
+  square.addEventListener("dragstart", dragStart)
+  square.addEventListener("dragover", dragOver)
+  square.addEventListener("drop", dragDrop)
+})
+
+let startPositionId
+let draggedElement
+
+function dragStart(e) {
+  startPositionId = e.target.parentNode.getAttribute("square-id")
+  draggedElement = e.target
+}
+
+function dragOver(e) {
+  e.preventDefault()
+}
