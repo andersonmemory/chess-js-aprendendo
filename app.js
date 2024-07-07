4cf5b2aac8b2dee6dffdf842c32bc006585aa85d
andersonmemory <andersonleitedesa2100@protonmail.com>
2024-07-07 15:46:12 -0300

    change colors of the board

--- app.js
+++ app.js
@@ -16,6 +16,19 @@ function createBoard() {
   board.forEach((piece, i) => {
     const square = document.createElement('div')
     square.classList.add('square')
+    square.setAttribute('id:', i)
+
+    const row = Math.floor(((63 - i) / 8) - 1)
+    if (row % 2 === 0)
+      {
+        
+        square.classList.add(i % 2 === 0? 'white-square': 'black-square') 
+      }
+      else {
+        square.setAttribute('row:', row)
+        square.classList.add(i % 2 === 0? 'black-square': 'white-square')
+      }
+
     square.innerHTML = piece
     gameBoard.append(square)
   })
